06c5a88fd84f430b7456470640108938207ceb11
Nathan Healea <nathan.healea@oregonstate.edu>
2016-01-19 21:37:29 -0800

    Update CardClassTest
    — testCardClassSetValue
       — test all values in a deck of cards
    — testCardClassSuit
      — test all suits in a deck of cards
    — Added buildCards
      — Helper function to reduce duplicated code.

--- Assignment1/src/test/java/models/CardClassTest.java
+++ Assignment1/src/test/java/models/CardClassTest.java
@@ -9,21 +9,68 @@ import static org.junit.Assert.*;
 public class CardClassTest {
 
     @Test
-    public void testCardClassSetValue(){
+    public void testCardClassValue(){
 
         // Arrange
-        Card c = new Card();
-        char received = ' ';
+        Card[] cards;
+        char[] values = {'A','2','3','4','5','6','7','8','9', 'T', 'J', 'Q', 'K'};
+        char[] received = new char[13];
 
         // Act
-        c.setValue('K');
-        received = c.getValue();
+
+        // Create cards
+        cards = buildCards(13);
+
+        // Setting cards values
+        for(int i = 0; i < values.length; i++){
+            cards[i].setValue(values[i]);
+        }
+
+        // Receiving values from cards
+        for(int i = 0; i < values.length; i++){
+            received[i] = cards[i].getValue();
+        }
 
         // Assert
-        assertSame('K', received);
-
+        assertArrayEquals(values, received);
 
     }
+
+    @Test
+    public void testCardClassSuit(){
+
+        // Arrange
+        Card[] cards;
+        char[] suit = {'S','C','H','D'};
+        char[] received = new char[4];
+
+        // Act
+
+        // Create cards
+        cards = buildCards(4);
+
+        // Setting cards values
+        for(int i = 0; i < suit.length; i++){
+            cards[i].setSuit(suit[i]);
+        }
+
+        // Receiving values from cards
+        for(int i = 0; i < suit.length; i++){
+            received[i] = cards[i].getSuit();
+        }
+
+        // Assert
+        assertArrayEquals(suit, received);
+
+    }
+
+    public Card[] buildCards(int count){
+        Card[] cards = new Card[count];
+        for(int i = 0; i < cards.length; i++){
+            cards[i] = new Card();
+        }
+        return cards;
+    };
 
 }
 
