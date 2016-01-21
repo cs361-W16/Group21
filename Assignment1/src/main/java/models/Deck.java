650950d14c91945e3bc34aeb3edfa6032829214a
foulgerd <foulgerd@oregonstate.edu>
2016-01-20 20:26:52 -0800

    Added Deck Class and DeckClassTest
    Just a quick creation of classes.

---
+++ Assignment1/src/main/java/models/Deck.java
@@ -0,0 +1,9 @@
+package models;
+
+/**
+ * Created by Devin on 1/20/2016.
+ */
+
+public class Deck {
+    
+}
---
+++ Assignment1/src/test/java/models/DeckClassTest.java
@@ -0,0 +1,11 @@
+package models;
+
+import org.junit.Test;
+
+/**
+ * Created by Devin on 1/20/2016.
+ */
+
+@Test
+public class DeckClassTest {
+}
