ecdd3be60a8eb8e711f33030da87ad0ddd817a41
Nathan Healea <nathan.healea@oregonstate.edu>
2016-01-22 11:39:43 -0800

    Update AcesUpControllerTest -- testPOSTDeal

--- Assignment1/src/test/java/controllers/AcesUpConrollerTest.java
+++ Assignment1/src/test/java/controllers/AcesUpConrollerTest.java
@@ -14,8 +14,24 @@ import static org.junit.Assert.assertEquals;
 public class AcesUpConrollerTest extends NinjaTest {
 
     String URL_DELETE_CARD = "/deleteCard";
+    String URL_DEAL_CARD = "/dealCard";
+
     @Test
     public void testPOSTDeleteCard(){
+        String id = "col-one";
+
+        String response = ninjaTestBrowser.postJson("http://localhost:8080" + URL_DELETE_CARD,id);
+
+        try {
+            String resultString = new ObjectMapper().readValue(response, String.class);
+            assertEquals("col-one",resultString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @Test
+    public void testPOSTDealCard(){
         String id = "col-one";
 
         String response = ninjaTestBrowser.postJson("http://localhost:8080" + URL_DELETE_CARD,id);
