01bddc97859e217fbfb6310b3ae2f60910b40c5e
Nathan Healea <nathan.healea@oregonstate.edu>
2016-01-22 02:45:44 -0800

    -- Update AcesUp   -- Add selector for all remove buttons. -- Update AcesUp View   -- Changed id to col-*   -- Add name to all buttons

--- Assignment1/src/main/java/assets/javascript/AcesUp.js
+++ Assignment1/src/main/java/assets/javascript/AcesUp.js
@@ -3,6 +3,23 @@
  */
 
 $(function(){
-
+    /**
+     * Btn Column One (remove)
+     */
+    $("[name='btn-remove']").click(function(){
+        var id = this.id;
+        alert(id);
+        $.ajax({
+            type: "POST",
+            url : "/deleteCard",
+            data: JSON.serialize(id)
+        })
+            .done(function(){
+                alert("Success");
+            })
+            .fail(function(){
+                alert("Ajax failed.");
+            })
+    });
 
 });
